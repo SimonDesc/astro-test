6c84f39b1e9161f37d02aa77941a691153931c24
Descombes Simon <pro.descombes@gmail.com>
2024-12-08 20:41:12 +0100

    feat: SSR mod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
+import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+    output: 'server',
+    adapter: node({
+        mode: 'standalone'
+	}),
 	integrations: [mdx(), sitemap()],
 });
+
+
